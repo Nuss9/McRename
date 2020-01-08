55441634ebc15f3fc94885938185c7935c302fd1
Nuss <nuss@Nusss-MacBook-Air.local>
2020-01-08 22:02:00 +0100

    Rename initial classes & file names

--- Renamer/BatchRenamer.cs
+++ Renamer/BatchRenamer.cs
@@ -2,7 +2,7 @@
 
 namespace Renamer
 {
-    public class Class1
+    public class BatchRenamer
     {
     }
 }
--- RenamerTests/BatchRenamerTests.cs
+++ RenamerTests/BatchRenamerTests.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace RenamerTests
 {
-    public class UnitTest1
+    public class BatchRenamertests
     {
         [Fact]
         public void Test1()

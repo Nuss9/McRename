2f86ea4117e2837befc8e1a73c2a76737c3d4c03
Dennis Jakupovic <dennisjakupovic@protonmail.ch>
2020-03-08 07:29:43 +0100

    Cut existing PathRewriter tests

--- Renamer/PathRewriter.cs
+++ Renamer/PathRewriter.cs
@@ -5,22 +5,6 @@ namespace Renamer
 {
     public class PathRewriter
     {
-		public string Execute(string path, DateTime fileCreationDateTime)
-		{
-			if(IsDirectory(path)) { return null; }
 
-			string creationDateTime = fileCreationDateTime.ToString("yyyyMMdd");
-			string creationDate = creationDateTime.Substring(0,8);
-			string directoryPath = Path.GetDirectoryName(path);
-			string extension = Path.GetExtension(path);
-			string result = directoryPath + "/" + creationDate + extension;
-
-			return result.Replace("\\", "/");
-		}
-
-		private static bool IsDirectory(string path)
-		{
-			return !Path.HasExtension(path);
-		}
 	}
 }
--- RenamerTests/PathRewriterTests.cs
+++ RenamerTests/PathRewriterTests.cs
@@ -9,26 +9,9 @@ namespace RenamerTests
 		public PathRewriter subject = new PathRewriter();
 
 		[Fact]
-		public void WhenInputFileDoesNotHaveAnExtension_ItReturnsNull()
+		public void When_ItShould()
 		{
-			string inputPath = "/Users/Me";
-			DateTime creationDate = new DateTime(2000, 12, 31);
 
-			string result = subject.Execute(inputPath, creationDate);
-
-			Assert.Null(result);
 		}
-
-        [Fact]
-        public void WhenRenamingToDate_ItShouldFormatToCreationDate()
-        {
-			string expected = "/Users/Me/20001231.txt";
-			string inputPath = "/Users/Me/file.txt";
-			DateTime creationDate = new DateTime(2000, 12, 31);
-
-			string result = subject.Execute(inputPath, creationDate);
-
-			Assert.Equal(expected, result);
-        }
     }
 }

a9d80f23e60b88e2b8700b00ba9a119fab8ee1b7
Dennis Jakupovic <djakupovic@winbase.nl>
2020-03-20 06:56:03 +0100

    Cut excessive property extension from FileInformation

--- Renamer/Dto's/FileInformation.cs
+++ Renamer/Dto's/FileInformation.cs
@@ -5,13 +5,11 @@ namespace Renamer
 	public class FileInformation
 	{
 		public string Path { get; }
-		public string Extension { get; }
 		public DateTime CreationDateTime { get; }
 
 		public FileInformation(string path, string extension, DateTime creationDateTime)
 		{
 			Path = path;
-			Extension = extension;
 			CreationDateTime = creationDateTime;
 		}
 	}

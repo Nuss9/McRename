ca4619e9a284375ebdadf571f7d0b001d92b21f5
Nuss @ Ubuntu <dennisjakupovic@protonmail.ch>
2020-03-29 08:10:38 +0200

    Cut excessive using statements

--- src/ConsoleInterface/Program.cs
+++ src/ConsoleInterface/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Renamer;
 
 namespace ConsoleInterface

62f867f80a4a8c4e4af4393dfc794f17ea3c4ca8
Dennis Jakupovic <dennisjakupovic@protonmail.ch>
2020-05-28 21:26:11 +0200

    Add test for composition output validation

--- src/Renamer/CompositionValidator.cs
+++ src/Renamer/CompositionValidator.cs
@@ -1,8 +1,20 @@
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Renamer
 {
     public class CompositionValidator
     {
-        
+        public (bool isValid, string errorMessage) Validate(Dictionary<string, string> composition)
+        {
+            if (composition.Values.Distinct().Count() == composition.Values.Count())
+            {
+                return (true, string.Empty);
+            }
+
+            return (false, "Duplicate filenames generated.");
+        }
     }
 }
--- src/RenamerTests/CompositionValidatorTests.cs
+++ src/RenamerTests/CompositionValidatorTests.cs
@@ -1,9 +1,26 @@
+using System.Collections.Generic;
+using Renamer;
 using Xunit;
 
 namespace RenamerTests
 {
     public class CompositionValidatorTests
     {
-        
+        [Fact]
+        public void WhenCompositionContainsDuplicateOutputFiles_ItShouldReturnAnError()
+        {
+            var composition = new Dictionary<string, string>
+			{
+				{ $"/Users/JohnDoe/Desktop/filea.txt", $"/Users/JohnDoe/Desktop/fileA.png"},
+				{ $"/Users/JohnDoe/Desktop/fileb.txt", $"/Users/JohnDoe/Desktop/fileA.png"},
+				{ $"/Users/JohnDoe/Desktop/filec.txt", $"/Users/JohnDoe/Desktop/fileC.png"},
+			};
+
+            var validator = new CompositionValidator();
+            var result = validator.Validate(composition);
+
+            Assert.False(result.isValid);
+            Assert.NotEmpty(result.errorMessage);
+        }
     }
 }

76f0beefa2b067ab4f23129f321ffc24dc1f4ebb
Dennis Jakupovic <dennisjakupovic@protonmail.ch>
2020-06-09 13:44:50 +0200

    Cut progress in Orchestrator class to start over

--- src/Renamer/RenameOrchestrator.cs
+++ src/Renamer/RenameOrchestrator.cs
@@ -5,26 +5,10 @@ namespace Renamer
 {
     public class RenameOrchestrator : IOrchestrate
     {
-        public IValidateComposeInstructions inputValidator;
-        public object composer;         // Needs to be private
-        public object outputValidator; // Needs to be private
-
-        public RenameOrchestrator(
-            IValidateComposeInstructions inputValidator,
-            ICompose composer,
-            IValidateComposeInstructions outputValidator
-            )
-        {
-            this.inputValidator = inputValidator;
-            this.composer = composer;
-            this.outputValidator = outputValidator;
-        }
 
         public Dictionary<string, string> Orchestrate(ComposeInstructions instructions)
         {
             var composition = new Dictionary<string, string>();
-
-            var validation = inputValidator.Validate(ref instructions);
 
             return composition;
         }
--- src/RenamerTests/RenameOrchestratorTests.cs
+++ src/RenamerTests/RenameOrchestratorTests.cs
@@ -1,39 +1,11 @@
-﻿using Renamer;
-using NSubstitute;
+﻿using NSubstitute;
+using Renamer;
 using Xunit;
-using System.Collections.Generic;
 
 namespace RenamerTests
 {
     public class RenameOrchestratorTests
     {
-        readonly RenameOrchestrator subject;
-
-        public RenameOrchestratorTests()
-        {
-            var inputValidator = Substitute.For<IValidateComposeInstructions>();
-            var composer = Substitute.For<ICompose>();
-            var outputValidator = Substitute.For<IValidateComposeInstructions>();
-
-            subject = new RenameOrchestrator(inputValidator, composer, outputValidator);
-        }
-
-        [Fact]
-        public void WhenInstantiated_ItShouldContainItsDependencies()
-        {
-            Assert.NotNull(subject.inputValidator);
-            Assert.NotNull(subject.composer);
-            Assert.NotNull(subject.outputValidator);
-        }
-
-        [Fact]
-        public void WhenOrchestratingInvalidInstructions_ItShouldCallInputValidatorOnce()
-        {
-            var instructions = new ComposeInstructions(ComposeMode.Numerical, new List<FileInformation>());
-
-            _ = subject.Orchestrate(instructions);
-
-            subject.inputValidator.Received(1).Validate(ref instructions);
-        }
+        
     }
 }
